96f8a9cefd89c45075704433eae4237cdf22eccc
Igor Bonato <igorbonato99@hotmail.com>
2021-04-20 18:01:35 -0300

    chang2

--- gamezin/0guess.py
+++ gamezin/0guess.py
@@ -1,28 +1,24 @@
 import random
-n = random.randint(1, 20)
-attempts = 0
+n = random.randint(1, 50)
+attempts = 1
 totalGuesses = 10
 
 print("LITTLE FUCKING GUESSING GAME\n")
-print("********************************************\ngenerating random number between 0 to 20...")
+print("********************************************\ngenerating random number between 0 to 50...")
 print(
     f'number generated!\nyoull have {totalGuesses} chances...\n********************************************\n')
 
-while True:
-    guess = int(input(f'Attempt number{attempts}'))
-    if guess < n:
-        print("You missed! Shoot higher!\n\n")
-        # attempts = attempts + 1
-    elif guess > n:
-        print("You missed! Shoot lower!\n\n")
-        # attempts = attempts + 1
-    elif guess != n and attempts == 10:
-        print(f'You tried {attempts} times and lost! You suck!')
-        break
-    else:
+while attempts < totalGuesses:
+    guess = int(input(f'Attempt number {attempts}: '))
+    attempts = attempts + 1
+    if guess == n:
         print("guessed!\nyou're great!\n")
         print(f'tries until guess: {attempts}')
         break
-
-for i in range(attempts, 0, +1):
-    if
+    elif guess > n:
+        print("You missed! Shoot lower!\n\n")
+    elif guess < n:
+        print("You missed! Shoot higher!\n\n")
+    else:
+        print(f'You tried {attempts} times and lost! You suck!')
+        break
