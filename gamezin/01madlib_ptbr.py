92887f7b8464ea56ec942fa97c3da24b40ee67dc
Igor Bonato <igorbonato99@hotmail.com>
2021-04-21 19:57:01 -0300

    mad

---
+++ gamezin/01madlib_ptbr.py
@@ -0,0 +1,88 @@
+# version 1
+
+print('bem vindo ao meu madlib.')
+print('pf responda o mais corretamente possivel.')
+
+print('\n\nsing = singular\npl = plural\nfem = feminino\nmasc = masculino\n\n')
+
+name = input("seu nome: ")
+adj1 = input("um adjetivo (masc): ")
+adj2 = input("mais um adjetivo (masc): ")
+celebrity = input("uma celebridade masculina: ")
+celebrity2 = input("outra celebridade masculina: ")
+animal = input("um animal: ")
+verbo = input("um verbo: ")
+sub1 = input("um substantivo (fem): ")
+verbo2 = input("outro verbo: ")
+sub2 = input("outro substantivo (masc): ")
+pdc = input("parte do corpo humano (sing/fem): ")
+adj3 = input("um adjetivo (fem): ")
+adj4 = input("outro adjetivo: ")
+adj5 = input("mais um adjetivo: ")
+adj6 = input("outro adjetivo: ")
+sens = input("sensação (pl): ")
+pdc2 = input("parte do corpo humano (sing/masc): ")
+sub3 = input("um substantivo (fem): ")
+adverb = input("adverbio de intensidade: ")
+adj7 = input("ultimo adjetivo: ")
+lugar = input("lugar: ")
+man = input("nome masculino: ")
+
+print(
+    f'\n\noiiii me chamo {name} e MEU HOMEM ideal deveria, antes de tudo, ser muito {adj1} e {adj2}.')
+print(
+    f'ele deve ter um físico como {celebrity.upper()}, um perfil como {celebrity2.upper()} e a inteligência de um {animal}.')
+print(
+    f'deve ser educado e deve sempre se lembrar de {verbo} minha {sub1}, de {verbo2} seu {sub2} e pegar minha {pdc} ao atravessar a rua.')
+print(
+    f'ele deve se mover de maneira {adj3}, ter uma voz {adj4} e deve sempre se vestir de forma {adj5}.')
+print(
+    f'eu tbm gostaria que ele fosse um dançarino {adj6}, e quando estivermos sozinhos ele deveria sussurrar coisas {sens} no meu {pdc2} e segurar minha {sub3}.')
+print(
+    f'eu sei que é uma batalha {adverb} {adj7} encontrar ele. Agora estou aqui em {lugar}..e na verdade, o único homemm em quem consigo pensar é no {man.upper()} :cc')
+print("\n\nby igorbonato\nthank u\n")
+
+# version 2
+
+print('bem vindo ao meu madlib.')
+print('pf responda o mais corretamente possivel.')
+
+print('\n\nsing = singular\npl = plural\nfem = feminino\nmasc = masculino\n\n')
+
+name = input("seu nome: ")
+adj1 = input("um adjetivo (fem): ")
+adj2 = input("mais um adjetivo (fem): ")
+celebrity = input("uma celebridade (mulher): ")
+celebrity2 = input("outra celebridade (mulher): ")
+animal = input("um animal: ")
+verbo = input("um verbo: ")
+sub1 = input("um substantivo (fem): ")
+verbo2 = input("outro verbo: ")
+sub2 = input("outro substantivo (masc): ")
+pdc = input("parte do corpo humano (sing/fem): ")
+adj3 = input("um adjetivo (fem): ")
+adj4 = input("outro adjetivo: ")
+adj5 = input("mais um adjetivo: ")
+prof = input("profissao (fem):")
+adj6 = input("outro adjetivo: ")
+sens = input("sensação (pl): ")
+pdc2 = input("parte do corpo humano (sing/masc): ")
+sub3 = input("um substantivo (fem): ")
+adverb = input("adverbio de intensidade: ")
+adj7 = input("ultimo adjetivo: ")
+lugar = input("lugar: ")
+woman = input("nome feminino: ")
+
+print(
+    f'\n\nopa! me chamo {name} e minha Ela ideal deveria, antes de tudo, ser muito {adj1} e {adj2}.')
+print(
+    f'ela deve ter um físico como {celebrity.upper()}, um perfil como {celebrity2.upper()} e a inteligência de um {animal}.')
+print(
+    f'deve ser educada e deve sempre se lembrar de {verbo} minha {sub1}, de {verbo2} seu {sub2} e pegar minha {pdc} ao atravessar a rua.')
+print(
+    f'ela deve se mover de maneira {adj3}, ter uma voz {adj4} e deve sempre se vestir de forma {adj5}.')
+print(
+    f'eu tbm gostaria que ela fosse uma {prof} {adj6}, e quando estivermos sozinhos ela deveria sussurrar coisas {sens} no meu {pdc2} e segurar minha {sub3}.')
+print(
+    f'eu sei que é uma batalha {adverb} {adj7} encontrar Ela. Agora estou aqui em {lugar}..e na verdade, o única mulher em quem consigo pensar é na {woman.upper()} :cc')
+print("\n\nby igorbonato\nthank u\n")
